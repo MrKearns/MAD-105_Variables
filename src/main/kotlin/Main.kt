b5d5b534ff221ef51196d82d22152fe8bc132fed
MrKearns <jkearns631@students.mchenry.edu>
2023-01-18 13:41:14 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    var myByte = -17
+    var myShort = 16
+    var myInt = 15000
+    var myLong = 100000000000000000L
+    var myFloat = 20.53F
+    var myDouble = 1.87
+    var myBool = true
+    var myChar: Char  = 'a'
+
+    println("\nHere's a list of variable types and examples.")
+    println("$myByte is a byte")
+    println("$myShort is a short")
+    println("$myInt is a int")
+    println("$myLong is a long")
+    println("$myFloat is a float")
+    println("$myDouble is a double")
+    println("$myBool is a bool")
+    println("$myChar is a char\n")
+
+    println("Program arguments: ${args.joinToString()}")
+}
